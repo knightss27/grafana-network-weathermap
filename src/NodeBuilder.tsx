84ee4708786a2ec2119a21e22f90974dbc7f519f
knightss@catlin.edu <knightss@catlin.edu>
2021-02-02 00:40:41 -0800

    Somewhat working state for multiple nodes.

--- src/NodeBuilder.tsx
+++ src/NodeBuilder.tsx
@@ -1,7 +1,7 @@
 import React, {useEffect, useState} from 'react';
 import { css } from 'emotion';
 import { stylesFactory } from '@grafana/ui';
-import { Button, Input} from '@grafana/ui';
+import { Button, Input, InlineField, InlineFieldRow} from '@grafana/ui';
 import { StandardEditorProps } from '@grafana/data';
 
 interface Settings {
@@ -10,8 +10,8 @@ interface Settings {
 
 interface Node {
     ID: string;
-    POSITION: [number, number];
     LABEL?: string;
+    POSITION: [number, number];
     INFOURL?: string;
     ICON?: string;
     ICONHEIGHT?: string;
@@ -43,8 +43,15 @@ export const NodeBuilder = ({ item, value, onChange }: Props) => {
 
     const handleChange = (e: any, i: number) => {
         let weathermap: Weathermap = value;
-        weathermap.NODES[i].LABEL = e.currentTarget.value;
+        if (e.currentTarget.name == 'X') {
+            weathermap.NODES[i].POSITION[0] = parseInt(e.currentTarget.value);
+        } else if (e.currentTarget.name == 'Y') {
+            weathermap.NODES[i].POSITION[1] = parseInt(e.currentTarget.value);
+        } else {
+            weathermap.NODES[i][e.currentTarget.name] = e.currentTarget.value;
+        }
         onChange(weathermap);
+        // console.log(e.currentTarget, i);
     }
 
     const addNewNode = () => {
@@ -72,18 +79,51 @@ export const NodeBuilder = ({ item, value, onChange }: Props) => {
             
             {ready ? value.NODES.map((node, i) => {
                 return (
-                Object.keys(node as Object).map(key => {
-                    return (
-                        <Input
-                            value={node[key] !== undefined ? node[key] : ''}
-                            onChange={e => handleChange(e, i)}
-                            placeholder={item.settings?.placeholder || ''}
-                            type={"text"}
-                            css={""}
-                            className={styles.nodeLabel}
-                        />
-                    )
-                })
+                    <InlineFieldRow>
+                    {Object.keys(node as Object).map(key => {
+                        if (key == 'POSITION') {
+                            return (
+                                <React.Fragment>
+                                    <InlineField label={"X"}>
+                                        <Input
+                                            value={node[key][0] !== undefined ? node[key][0] : ''}
+                                            onChange={e => handleChange(e, i)}
+                                            placeholder={item.settings?.placeholder || ''}
+                                            type={"number"}
+                                            css={""}
+                                            className={styles.nodeLabel}
+                                            name={"X"}
+                                        />
+                                    </InlineField>
+                                    <InlineField label={"Y"}>
+                                        <Input
+                                            value={node[key][1] !== undefined ? node[key][1] : ''}
+                                            onChange={e => handleChange(e, i)}
+                                            placeholder={item.settings?.placeholder || ''}
+                                            type={"number"}
+                                            css={""}
+                                            className={styles.nodeLabel}
+                                            name={"Y"}
+                                        />
+                                    </InlineField>
+                                </React.Fragment>
+                            )
+                        }
+                        return (
+                            <InlineField label={key}>
+                                <Input
+                                    value={node[key] !== undefined ? node[key] : ''}
+                                    onChange={e => handleChange(e, i)}
+                                    placeholder={item.settings?.placeholder || ''}
+                                    type={"text"}
+                                    css={""}
+                                    className={styles.nodeLabel}
+                                    name={key}
+                                />
+                            </InlineField>
+                        )
+                    })}
+                    </InlineFieldRow>
                 )
             }): ''}
             
@@ -104,12 +144,11 @@ export const NodeBuilder = ({ item, value, onChange }: Props) => {
 const getStyles = stylesFactory(() => {
     return {
       nodeLabel: css`
-        margin: 10px 0px;
+        margin: 0px 0px;
       `,
       addNew: css`
         width: 100%;
         justify-content: center;
       `
-
     };
   });
