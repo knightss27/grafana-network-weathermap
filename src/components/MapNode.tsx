e5cbeada231db96ca430336e69f8413f1f03306c
Seth Knights <knightss@catlin.edu>
2022-04-18 18:49:59 -0700

    refactor(MapNode): clean up logs

--- src/components/MapNode.tsx
+++ src/components/MapNode.tsx
@@ -49,8 +49,6 @@ const MapNode: React.FC<NodeProps> = (props: NodeProps) => {
   const rectHeight = useMemo(() => calculateRectangleAutoHeight(node, wm), [node, wm]);
   const textY = useMemo(() => calculateTextY(node), [node]);
 
-  console.log('rendering node');
-
   return (
     <DraggableCore disabled={disabled} onDrag={onDrag} onStop={onStop}>
       <g
